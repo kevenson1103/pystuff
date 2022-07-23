a5e72f4698a6cfd223718aa390b8d2c2d014d93a
rellyman53 <nevenson2016@gmail.com>
2022-07-22 20:06:34 -0500

    ceil and flooring numbers

--- math.py
+++ math.py
@@ -8,6 +8,8 @@ x = math.sqrt(117)
 print(x)
 
 print(math.sqrt(81))
+print(math.sqrt(25))
+print(math.sqrt(73))
 
 # ---------------
 # flooring (rounding) numbers
@@ -20,3 +22,15 @@ print(y)
 print(math.floor(8.3))
 print(math.floor(9.5))
 print(math.floor(4.8))
+
+# ----------------
+# ceil - weird thing that gives you the highest value (ex: the ceil of 5.3 = 6)
+
+print('-- ceil --')
+
+z = math.ceil(math.sqrt(823))
+print(z)
+
+print(math.ceil(5.3))
+print(math.ceil(10.7))
+print(math.ceil(1.2))
