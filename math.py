b67f4def5a5732450fdafc87b165be44ffe5b6db
rellyman53 <nevenson2016@gmail.com>
2022-07-22 20:16:16 -0500

    more math

--- math.py
+++ math.py
@@ -23,10 +23,10 @@ print(math.floor(8.3))
 print(math.floor(9.5))
 print(math.floor(4.8))
 
-# ----------------
+# ---------------
 # ceil - weird thing that gives you the highest value (ex: the ceil of 5.3 = 6)
 
-print('-- ceil --')
+print('-- math.ceil --')
 
 z = math.ceil(math.sqrt(823))
 print(z)
@@ -34,3 +34,35 @@ print(z)
 print(math.ceil(5.3))
 print(math.ceil(10.7))
 print(math.ceil(1.2))
+
+# ---------------
+# squaring numbers
+
+print('-- squaring numbers --')
+
+a = 3 ** 2
+print(a)
+
+print(9 ** 8)
+print(4 ** 3)
+print(13 ** 4)
+
+# another way to square numbers, or set them to a power of something
+
+print('-- math.pow --')
+
+b = math.pow(3,2)
+print(b)
+
+print(math.pow(9,8))
+print(math.pow(4,3))
+print(math.pow(13,4))
+
+# ---------------
+# some random math functions
+
+print('-- some random math functions --')
+
+print(math.pi)
+print(math.e)
+print(math.inf)
